ecc42ca89e8587918ee2af22c7d52806e8f8ce6c
Salman <salman.karim@hotmail.com>
2015-08-01 12:36:10 +0100

    creates addTask function

--- js/toDoListController.js
+++ js/toDoListController.js
@@ -1,10 +1,19 @@
 toDoList.controller('ToDoListController', [function() {
 
-  this.tasklist = {
-    "tasks": [
+  this.taskList = [
       { "task": "Buy groceries" },
       { "task": "Do laundry" }
-    ]
+    ];
+  // };
+
+  this.addTask = function() {
+    this.taskList.push({
+      "task": this.newTask
+    });
+    console.log(this.taskList);
   };
 
 }]);
+
+
+// }]);
--- test/toDoListController.spec.js
+++ test/toDoListController.spec.js
@@ -9,27 +9,68 @@ describe("ToDoListController", function() {
     ctrl = $controller('ToDoListController');
   }));
 
-  it('initializes with an empty task list', function() {
-    expect(ctrl.taskList).toBeUndefined();
-    expect(ctrl.taskList).toBeUndefined();
-  });
-});
-
-describe('when viewing to do list', function() {
-  beforeEach(module('ToDoList'));
-
-  var ctrl;
-
-  beforeEach(inject(function($controller) {
-    ctrl = $controller('ToDoListController');
-  }));
-
   var tasks = [
     { "task": "Buy groceries" },
     { "task": "Do laundry" }
   ];
 
-  it('displays list of tasks', function() {
-    expect(ctrl.tasklist.tasks).toEqual(tasks);
+  // describe('upon creation', function() {
+  //
+  //   it('initializes with an empty task list', function() {
+  //     expect(ctrl.taskList).toBeUndefined();
+  //     expect(ctrl.taskList).toBeUndefined();
+  //   });
+  // });
+
+  describe('viewing list', function() {
+
+    it('displays list of tasks', function() {
+      expect(ctrl.taskList).toEqual(tasks);
+    });
   });
+
+  describe('adding tasks', function () {
+
+    it('displays list of tasks', function() {
+      ctrl.newTask = "Do the dishes";
+      ctrl.addTask();
+      expect(ctrl.taskList).toContain({'task': 'Do the dishes'});
+    });
+  });
+
 });
+
+
+//
+// describe('to do list', function() {
+//
+//   beforeEach(module('ToDoList'));
+//
+//   var ctrl;
+//
+//   beforeEach(inject(function($controller) {
+//     ctrl = $controller('ToDoListController');
+//   }));
+//
+//   var tasks = [
+//     { "task": "Buy groceries" },
+//     { "task": "Do laundry" }
+//   ];
+//
+//   context('viewing to do list', function () {
+//
+//     it('displays list of tasks', function() {
+//       expect(ctrl.tasklist.tasks).toEqual(tasks);
+//     });
+//   });
+//
+//   context('adding tasks', function () {
+//
+//     it('displays list of tasks', function() {
+//       expect(ctrl.addtask).toEqual(tasks);
+//     });
+//   });
+//
+//
+//
+// });
