1bb6f82be47ca196aaa30572ae7bae526f3c69a4
Salman <salman.karim@hotmail.com>
2015-08-02 14:45:23 +0100

    sets up protractor with sinatra server

--- test/e2e/toDoListFeature.js
+++ test/e2e/toDoListFeature.js
@@ -1,8 +1,18 @@
 describe('To Do List', function() {
 
   it('has a title', function() {
-    browser.get('http://localhost:8080');
+    browser.get('http://localhost:4567');
 
     expect(browser.getTitle()).toEqual("SK's to-do list");
   });
+
+  it('has a title', function() {
+    browser.get('http://localhost:4567');
+
+    element(by.model('toDoCtrl.newTask')).sendKeys('Do the dishes');
+    element(by.className('btn')).click();
+
+    expect(element.all(by.repeater('entry in toDoCtrl.taskList')).get(2).getText()).toEqual('Do the dishes');
+  });
+
 });
