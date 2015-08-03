fc081a3f7569ed9682ca0231bcc36bf3b341d81e
Salman <salman.karim@hotmail.com>
2015-08-03 10:55:06 +0100

    adds test for new task

--- test/e2e/conf.js
+++ test/e2e/conf.js
@@ -1,5 +1,10 @@
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['toDoListFeature.js'],
-
+  capabilities: {
+      browserName: 'firefox'
+    },
+  onPrepare: function() {
+    browser.driver.manage().window().maximize();
+  }
 };
--- test/e2e/toDoListFeature.js
+++ test/e2e/toDoListFeature.js
@@ -1,18 +1,33 @@
 describe('To Do List', function() {
 
-  it('has a title', function() {
-    browser.get('http://localhost:4567');
+  var newTaskBox = element(by.model('toDoCtrl.newTask'));
+  var tasks = element.all(by.repeater('entry in toDoCtrl.taskList'));
+  var enter = browser.actions().sendKeys(protractor.Key.ENTER);
 
+  beforeEach(function() {
+    browser.get('http://localhost:4567');
+  });
+
+
+  it('has a title', function() {
     expect(browser.getTitle()).toEqual("SK's to-do list");
   });
 
   it('has a title', function() {
-    browser.get('http://localhost:4567');
-
-    element(by.model('toDoCtrl.newTask')).sendKeys('Do the dishes');
+    element(newTaskBox.sendKeys('Do the dishes'));
     element(by.className('btn')).click();
 
-    expect(element.all(by.repeater('entry in toDoCtrl.taskList')).get(2).getText()).toEqual('Do the dishes');
+    expect(tasks.last().getText()).toEqual('Do the dishes');
   });
 
+  // // it('can edit a task', function() {
+  // //   element(by.binding('entry.task')).click();
+  // //   element(by.className('test')).sendKeys('Revised task');
+  // //   // element(by.binding('entry.task')).sendKeys('Revised task');
+  // //   enter.perform();
+  // //   // element(tasks.first()).click();
+  //
+  //
+  //   // expect(tasks.first().getText()).toEqual('Revised task');
+  // });
 });
